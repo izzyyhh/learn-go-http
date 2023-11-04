b7f24bf626aefba99f4441167fbc2e692a087949
Ismail Halili <i.halili99@hotmail.com>
2023-11-04 05:02:21 +0100

    add example consuming json rest chuck norris api, decode and encode

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type ChuckNorrisApiResponse struct {
+	Value string `json:"value"`
+}
+
+const RANDOM_CHUCK_NORRIS_API_URL = "https://api.chucknorris.io/jokes/random"
+
+var client = &http.Client{}
+
+func main() {
+	body := getBodyFromGetRequest()
+
+	// from json to struct
+	var chuckNorrisResponse ChuckNorrisApiResponse
+	err := json.Unmarshal(body, &chuckNorrisResponse)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(chuckNorrisResponse.Value)
+
+	// from struct to json
+	marshalled, _ := json.Marshal(chuckNorrisResponse)
+	fmt.Println(string(marshalled))
+
+}
+
+func getBodyFromGetRequest() []byte {
+	resp, err := client.Get(RANDOM_CHUCK_NORRIS_API_URL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body
+}
